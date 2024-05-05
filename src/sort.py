9337b7a04ee85dcf4f88649adafc199716341533
Pasubu.book <163115290+pasububook@users.noreply.github.com>
2024-05-05 20:17:19 +0900

    config.json の仕様の変更の対応

--- src/sort.py
+++ src/sort.py
@@ -15,15 +15,15 @@ def is_include(filename):
 
 # 写真を並べ替えるプログラム
 def PhotoSort_lite():
-    file_name_list = os.listdir(config_data["update_folder"])
+    file_name_list = os.listdir(config_data["folder"])
 
     # 画像、動画ファイルのリネーム
     for i in range(len(file_name_list)):
-        file_pass = config_data["update_folder"] + file_name_list[i]
+        file_pass = config_data["folder"] + file_name_list[i]
         if is_include(file_name_list[i]) != False:
             print("リネーム後のファイル名 :" + file_name_list[i])
             took_time = os.stat(file_pass).st_ctime
-            os.rename(file_pass, config_data["update_folder"] + str(took_time).replace(".", "") + "." + is_include(file_name_list[i]))
+            os.rename(file_pass, config_data["folder"] + str(took_time).replace(".", "") + "." + is_include(file_name_list[i]))
 
     # 画像、動画ファイルの移動
 
